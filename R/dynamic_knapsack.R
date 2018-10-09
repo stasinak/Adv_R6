74b37252524fe7372d23f34b5ae01e08059d008a
Stefano Toffol <stefanotoffol@gmail.com>
2018-10-09 14:14:15 +0200

    dynamic almost working

--- R/dynamic_knapsack.R
+++ R/dynamic_knapsack.R
@@ -1,49 +1,68 @@
-
-knapsack_dynamic <- function(x, W) {
+dynamic_knapsack <- function(x, W) {
   
-  if(!is.data.frame(x) | ncol(x)!=2) stop("The input object is not of data.frame type.\n")
+  if(!is.data.frame(x) | ncol(x)!=2) 
+    stop("The input object is not of data.frame type.\n")
   if(!(all(colnames(x)==c("v", "w")) | all(colnames(x)==c("w", "v"))))
     stop("The data.frame should have the columns named 'v' and 'w'.")
-  if(!is.numeric(W) | length(W)!=1 | W<=0) stop("The total weight (W) should be a positive scalar")
+  if(!is.numeric(W) | length(W)!=1 | W<=0) 
+    stop("The total weight (W) should be a positive scalar")
   
-  rownames(x) <- 1:nrow(x)
+  rownames(x) <- 1:nrow(x)  
+    # We give rownames so that after filtering we can acquire original positions of elements
   too_big <- which(x$w>W)
+    # Index of units with individual weight major than the threshold
   x <- x[-too_big,]
+    # Filtered dataset
   x <- x[order(x$w),]
+    # Order according to weight
   n <- nrow(x)
   
-  m <- matrix(0, n, W+1)
   s <- x$w[1]
-  for(i in 1:n) {
+    # To optimize calculation of m: it's going to be the sum of all the weights from 1 to i
+    # If the value of the weight in column J is greater than s then all remaining cells
+    # of row I will have the last value calculated for that row
+  m <- matrix(0, n, W+1)
+  first_line <- c(rep(0, x$w[1]), rep(x$v[1], (W+1)-x$w[1]))
+    # We initialize the first line
+  m[1,] <- first_line
+  
+  for(i in 2:n) {
+    
     for(j in 2:W+1) {
+      
       if( (j-1)>s ) {
         m[i, j:W+1] <- m[i, j-1]
         s <- ifelse(i<n, s+x$w[i], s)
         break
       }
-      if( x$w[i] > (j-1) ) 
-        m[i, j] <- m[i, j-1]
-      else 
+      
+      if( x$w[i] > (j-1) )
+        m[i, j] <- m[i-1, j]
+      else
         m[i, j] <- max(m[i-1, j], m[i-1, j-x$w[i]] + x$v[i])
+      
     }
   }
   
-  # i <- n
-  # j <- W
-  # ind <- c()
-  # 
-  # while(i!=1 | j!=1) {
-  #   print(i, j)
-  #   if(m[i, j] == m[i-1, j]) i <- i-1
-  #   else {
-  #     ind <- c(ind, i)
-  #     j <- j - x$w[i]
-  #     i <- i-1
-  #   }
-  # }
-  # 
-  # return(value = max(m), elements = sort(as.numeric(rownames(x[ind,]))))
-  return(m)
+  # Find optimal values
+  i <- n
+  j <- W+1
+  ind <- c()
+
+  while(j>0) {
+    print(c("i:", i, "; j:", j))
+    if(i>1 && m[i, j] == m[i-1, j]) i <- i-1
+    else {
+      ind <- c(ind, i)
+      j <- j - x$w[i]
+      i <- ifelse(i>1, i-1, i)
+    }
+  }
+
+  return(list(value = max(m), elements = sort(as.numeric(rownames(x)[ind]))))
 }
 
-knapsack_dynamic(x = knapsack_objects[1:8, ], 3500)
+dynamic_knapsack(knapsack_objects[1:8, ], 3500)
+dynamic_knapsack(x = knapsack_objects[1:12,], W = 3500)
+dynamic_knapsack(x = knapsack_objects[1:8,], W = 2000)
+dynamic_knapsack(x = knapsack_objects[1:12,], W = 2000)
